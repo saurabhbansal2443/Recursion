45d7ee2712d7b6995157b269ebe75a630645f919
Saurabh Bansal <saurabhbansal2443@gmail.com>
2023-06-17 17:17:01 +0530

    get subsequence

---
+++ getSubsequence.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+
+public class getSubsequence {
+    public static void main(String[] args) {
+        String str = "abc";
+
+        ArrayList<String> res = subsequence(str);
+
+        System.out.println(res);
+
+    }
+
+    public static ArrayList<String> subsequence(String str) {
+
+        if (str.length() == 0) {
+            ArrayList<String> base = new ArrayList<>();
+            base.add("");
+            return base;
+        }
+
+        char ch = str.charAt(0);
+        String rem = str.substring(1);
+
+        ArrayList<String> rres = subsequence(rem);
+
+        ArrayList<String> ans = new ArrayList<>();
+
+        for (String s : rres) {
+
+            ans.add(s);
+            ans.add(ch + s);
+        }
+
+        return ans;
+
+    }
+}
